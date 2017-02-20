f1d91b41a9b14ea822ce697d1fb73f790dcc86af
Kelly Selden <kellyselden@gmail.com>
2017-02-19 18:07:30 -0800

    fix id bug

--- index.js
+++ index.js
@@ -11,41 +11,23 @@ console.log('started on port ' + port);
 wss.on('connection', function connection(ws) {
   console.log('connection');
   let id = uuidV4();
-  // clients[ws] = id;
-  // wss.clients.forEach(function each(client) {
-  //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-  //     client.send(id + ' ' + JSON.stringify({
-  //       type: 'id'
-  //     }));
-  //   }
-  //   ws.send(id + ' ' + JSON.stringify({
-  //     type: 'id'
-  //   }));
-  // });
   clients[id] = ws;
-  Object.keys(clients).forEach(id => {
-    let client = clients[id];
+  Object.keys(clients).forEach(clientId => {
+    let client = clients[clientId];
     if (client !== ws && client.readyState === WebSocket.OPEN) {
       client.send(id + ' ' + JSON.stringify({
         type: 'id'
       }));
+      ws.send(clientId + ' ' + JSON.stringify({
+        type: 'id'
+      }));
     }
-    ws.send(id + ' ' + JSON.stringify({
-      type: 'id'
-    }));
   });
 
   ws.on('message', function incoming(data) {
     // Broadcast to everyone else.
     // console.log('message: ', data);
-    // wss.clients.forEach(function each(client) {
-    //   if (client !== ws && client.readyState === WebSocket.OPEN) {
-    //     console.log('send: ', data);
-    //     client.send(clients[client] + ' ' + data);
-    //   }
-    // });
-    Object.keys(clients).forEach(id => {
-      let client = clients[id];
+    wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         // console.log('send: ', data);
         client.send(id + ' ' + data);
@@ -56,8 +38,7 @@ wss.on('connection', function connection(ws) {
   ws.on('close', function() {
     console.log('close: ' + id);
     delete clients[id];
-    Object.keys(clients).forEach(id => {
-      let client = clients[id];
+    wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(id + ' ' + JSON.stringify({
           type: 'left'
