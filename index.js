58f3a75bd2fd94e603c21a0e4d5a4c1af2549dd2
Kelly Selden <kellyselden@gmail.com>
2017-02-19 22:24:05 -0800

    route optimizing

--- index.js
+++ index.js
@@ -5,44 +5,104 @@ let port = process.env.PORT || 8080;
 
 const wss = new WebSocket.Server({ port });
 
-let clients =  [];
+let clients = {};
+
+function applyId(id, data) {
+  let json = JSON.parse(data);
+  json.id = id;
+  data = JSON.stringify(json);
+  return data;
+}
+
+// setInterval(() => {
+
+// }, 10000);
 
 console.log('started on port ' + port);
 wss.on('connection', function connection(ws) {
   console.log('connection');
   let id = uuidV4();
-  clients[id] = ws;
-  Object.keys(clients).forEach(clientId => {
-    let client = clients[clientId];
-    if (client !== ws && client.readyState === WebSocket.OPEN) {
-      client.send(id + ' ' + JSON.stringify({
-        type: 'id'
-      }));
-      ws.send(clientId + ' ' + JSON.stringify({
-        type: 'id'
-      }));
-    }
-  });
+  clients[id] = {
+    ws
+  };
+  // let dataForOthers = JSON.stringify({
+  //   type: 'id',
+  //   id
+  // });
+  // Object.keys(clients).forEach(clientId => {
+  //   let client = clients[clientId].ws;
+  //   if (client !== ws && client.readyState === WebSocket.OPEN) {
+  //     client.send(dataForOthers);
+  //     ws.send(JSON.stringify({
+  //       type: 'id',
+  //       id: clientId
+  //     }));
+  //   }
+  // });
 
   ws.on('message', function incoming(data) {
     // Broadcast to everyone else.
     // console.log('message: ', data);
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        // console.log('send: ', data);
-        client.send(id + ' ' + data);
-      }
-    });
+    let json = JSON.parse(data);
+    json.id = id;
+    if (json.type === 'route') {
+      let previousRoute = clients[id].route;
+      clients[id].route = json.data;
+      Object.keys(clients).forEach(clientId => {
+        let client = clients[clientId];
+        if (client.ws !== ws && client.ws.readyState === WebSocket.OPEN) {
+          data = JSON.stringify(json);
+          // console.log('send: ', data);
+          if (client.route === previousRoute) {
+            // client.ws.send(data);
+            client.ws.send(JSON.stringify({
+              type: 'left',
+              id
+            }));
+          }
+          if (client.route === json.data) {
+            // console.log('me: ' + id + ' with route ' + json.data);
+            // console.log('other: ' + clientId + ' with route ' + client.route);
+            ws.send(JSON.stringify({
+              type: 'join',
+              id: clientId,
+              data: client.route
+            }));
+            client.ws.send(JSON.stringify({
+              type: 'join',
+              id,
+              data: client.route
+            }));
+          }
+        }
+      });
+    } else {
+      Object.keys(clients).forEach(clientId => {
+        let client = clients[clientId];
+        if (client.ws !== ws && client.ws.readyState === WebSocket.OPEN) {
+          if (client.route === clients[id].route) {
+            data = JSON.stringify(json);
+            // console.log('send: ', data);
+            client.ws.send(data);
+          }
+        }
+      });
+    }
   });
 
   ws.on('close', function() {
     console.log('close: ' + id);
+    let route = clients[id].route;
     delete clients[id];
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(id + ' ' + JSON.stringify({
-          type: 'left'
-        }));
+    Object.keys(clients).forEach(clientId => {
+      let client = clients[clientId];
+      if (client.readyState === WebSocket.OPEN) {
+        if (client.route === route) {
+          client.send(JSON.stringify({
+            type: 'left',
+            id
+          }));
+        }
       }
     });
   });
